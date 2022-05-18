cdac4fca0e5a895fc3b1aa1f12a7869b2adefca5
Pedro Lucas Gomes <pedro.lg.oliveira@edu.ufes.br>
2022-05-18 02:28:43 -0300

    second commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,12 @@
+import Head from "next/head";
 import styles from "../styles/home.module.scss";
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Início | ig.news</title>
+      </Head>
       <h1 className={styles.title}>Hello Word</h1>
       <h2>dadad</h2>
     </>
